07e2fbd4c01905ca58be160701134143e8f0ddd8
Euler Barbosa <eulerbarbosa96@gmail.com>
2023-01-30 20:38:11 -0300

    Acrescentando a consulta de chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,6 +1,24 @@
 <?php
-  require_once('validador_acesso.php') 
+  require_once('validador_acesso.php') ;
 ?>
+
+<?php
+  //abre o arquivo para leitura
+  $arquivo = fopen('arquivo.hd','r');
+
+  //array para armazenar os chamados
+  $chamados = array();
+
+  //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)){//testa pelo fim de um arquivo
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  //fecha o arquivo aberto
+  fclose($arquivo);
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -42,23 +60,23 @@
             
             <div class="card-body">
               
+              <?php 
+                foreach($chamados as $chamado){
+                  $chamado_dados = explode('#',$chamado);
+                  if(count($chamado_dados)<3){
+                    continue;
+                  }
+              ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
+                  <h5 class="card-title"><?=$chamado_dados[0]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[1]?></h6>
+                  <p class="card-text"><?=$chamado_dados[2]?></p>
                 </div>
               </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php 
+                }
+              ?>
 
               <div class="row mt-5">
                 <div class="col-6">
